bd54bb6442e808895aa04ee6639462d3d8f6cf2e
Komar0ff <binary.reptile@gmail.com>
2018-05-19 01:04:29 +0300

    add index

---
+++ client/src/components/Input/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'react-emotion'
+
+
+const Wrapper = styled('div')`
+
+`
+
+const _Input = styled('input')`
+  
+`
+
+const Input = (link) => {
+
+  return <_Input 
+            placeholder = 'Enter repo link'/>
+
+}
+
+
+export default Input
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,19 @@ import ReactDOM from 'react-dom'
 
 //import this
 
+
+
 const App = () => {
   return (
     // here add component
-    <div>test</div> // this delete
   )
 }
 
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept();
+}
