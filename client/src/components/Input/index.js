f8c29b53f168b923c98e8a7d3cdb2657a8878ffc
Komar0ff <binary.reptile@gmail.com>
2018-11-09 17:15:10 +0300

    refactoring Input

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -2,29 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'react-emotion';
 
-const Wrapper = styled('div')`
-  display: flex;
-  justify-content: center;
-`;
+const InputBox = styled('input')`
+  margin: auto;
+  padding: 20px;
+  width: 400px;
 
-const Input = styled('input')`
   border: 1px solid #ccc;
-  min-width: 200px;
   border-radius: 4px;
-  padding: 10px;
   outline: none;
+
+  font-size: 21px;
+  transition: ease-out 0.5s;
+
+  &:focus {
+    box-shadow: 4px 4px 9px rgba(0, 20, 40, 0.1);
+    transition: ease-out 0.2s;
+  }
 `;
 
-export default (props) => {
+const Input = (props) => {
   return (
-    <Wrapper>
-      <Input
-        placeholder="Enter repo link"
-        type="url"
-        onChange={(e) => {
-          props.onChange(e.target.value);
-        }}
-      />
-    </Wrapper>
-  );
-};
+    <InputBox
+      placeholder="Enter repo link"
+      type="url"
+      onChange={(e) => {
+        props.onChange(e.target.value);
+      }}
+    />
+  )
+}
+
+export default Input
